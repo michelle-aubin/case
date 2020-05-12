4fc44b31e5624d9e10654d3181edbc3891195d69
Michelle Aubin <phiangda@ualberta.ca>
2020-05-12 15:17:26 -0600

    cleaned up

--- format_training_data.py
+++ format_training_data.py
@@ -1,53 +1,68 @@
 import spacy
 import json
 
-def handle_title(title, ents_data, data_list, char_count):
+def handle_title(title, ents_data, data_list):
     ents = []
     length = len(title)
-    print("Title is: %s" % title)
-    print("Length of title is: %d" % length)
+    start_ind = 0
     for ent in ents_data:
-        if ent.get('start') < length:
-            ents.append((ent.get('start'), ent.get('end'), ent.get('type')))
-            print("Added %s with start %d and end %d" % (ent.get('text'), ent.get('start') - char_count, ent.get('end') - char_count))
+        ent_text = ent.get('text')
+        ind = title.find(ent.get('text'), start_ind)
+        end = ind + len(ent_text)
+        # entity is in the sentence
+        if ind != -1:
+            ents.append((ind, end, ent.get('type')))
+            start_ind = end
+        # entity is not in the sentence
+        # take all found entities out and return
         else:
-        #    print("entity is ", ent)
-            ents_data = ents_data[ents_data.index(ent):]
-            break
-    data_list.append((title, {'entities': ents}))
-    return char_count + length + 2, ents_data
+            return ents_data[ents_data.index(ent):]
 
-def handle_abstract(abstract, ents_data, data_list, char_count):
-    for sent in abstract.sents:
-        print(sent)
 
-def handle_body(body, ents_data, data_list, char_count):
-    for s in body.sents:
+def handle_abstract(abstract, ents_data, data_list):
+    for s in abstract.sents:
         ents = []
         sent = s.text
-        length = len(sent)
-    #    print("Sentence is: %s" % sent)
-       # print("Sentence is %d chars long" % length)
-       # print("Num of chars before sentence is: %d" % char_count)
         start_ind = 0
         for ent in ents_data:
             ent_text = ent.get('text')
-       #     print("Looking for %s starting at %d" % (ent_text, start_ind))
             ind = sent.find(ent.get('text'), start_ind)
             end = ind + len(ent_text)
             # entity is in the sentence
             if ind != -1:
                 ents.append((ind, end, ent.get('type')))
                 start_ind = end
-     #           print("Added %s with start %d and end %d" % (ent_text, ind, end))
+            # entity is not in the sentence
+            # take all found entities out and search next sentence
             else:
-                #print("entity is ", ent)
                 ents_data = ents_data[ents_data.index(ent):]
                 break
-        #char_count += length + 1
+        # add to training data
         data_list.append((sent, {'entities': ents}))
-    for data in data_list:
-        print(data)
+    return ents_data
+
+
+def handle_body(body, ents_data, data_list):
+    for s in body.sents:
+        ents = []
+        sent = s.text
+        start_ind = 0
+        for ent in ents_data:
+            ent_text = ent.get('text')
+            ind = sent.find(ent.get('text'), start_ind)
+            end = ind + len(ent_text)
+            # entity is in the sentence
+            if ind != -1:
+                ents.append((ind, end, ent.get('type')))
+                start_ind = end
+            # entity is not in the sentence
+            # take all found entities out and search next sentence
+            else:
+                ents_data = ents_data[ents_data.index(ent):]
+                break
+        # add to training data
+        data_list.append((sent, {'entities': ents}))
+
     
 
 def main():
@@ -64,30 +79,8 @@ def main():
             body = data.get('body')
             abs_doc = nlp(abstract)
             body_doc = nlp(body)
-            char_count, ents_data = handle_title(title, ents_data, data_list, char_count)
-            handle_abstract(abs_doc, ents_data, data_list, char_count)
-            handle_body(body_doc, ents_data, data_list, char_count)
-
-
-            # for data in data_list:
-            #     print(data)
-            break
+            ents_data = handle_title(title, ents_data, data_list)
+            ents_data = handle_abstract(abs_doc, ents_data, data_list)
+            handle_body(body_doc, ents_data, data_list)
 
 main()
-
-"""
-    char count = 0
-    for each sentence:
-        len = num of chars in sentence
-        if its the first sentence:
-            store sentence and entities with offsets in data list
-            char count += len
-        else:    
-            find entities that have a start offset between 
-            char count and char count + len (its in the sentence)
-            new offset for each entity is offset - char count
-            store sentence and entities with new offsets in data list
-
-        
-
-"""
