b261cdc0798fab0e6505d66ddaceb3c2e3c701da
Michelle Aubin <phiangda@ualberta.ca>
2020-05-12 16:21:38 -0600

    prints 'added labels'

--- train_ner.py
+++ train_ner.py
@@ -33,9 +33,10 @@ LABELS_FILE = 'labels.txt'
     n_iter=("Number of training iterations", "option", "n", int),
 )
 def main(train_file, model=None, output_dir=None, n_iter=100):
-    # Loading training data 
-    # with open (train_file, 'rb') as fp:
-    #     train_data = pickle.load(fp)
+    # load training data 
+    with open (train_file, 'rb') as fp:
+        train_data = pickle.load(fp)
+        print("Loaded training data '%s'" % train_file)
 
     """Load the model, set up the pipeline and train the entity recognizer."""
     if model is not None:
@@ -45,65 +46,65 @@ def main(train_file, model=None, output_dir=None, n_iter=100):
         nlp = spacy.blank("en")  # create blank Language class
         print("Created blank 'en' model")
 
-    # create the built-in pipeline components and add them to the pipeline
-    # nlp.create_pipe works for built-ins that are registered with spaCy
-    # if "ner" not in nlp.pipe_names:
-    #     ner = nlp.create_pipe("ner")
-    #     nlp.add_pipe(ner, last=True)
-    # # otherwise, get it so we can add labels
-    # else:
-    #     ner = nlp.get_pipe("ner")
+   # create the built-in pipeline components and add them to the pipeline
+   # nlp.create_pipe works for built-ins that are registered with spaCy
+    if "ner" not in nlp.pipe_names:
+        ner = nlp.create_pipe("ner")
+        nlp.add_pipe(ner, last=True)
+    # otherwise, get it so we can add labels
+    else:
+        ner = nlp.get_pipe("ner")
 
-    # # add labels
-    # lf = open(LABELS_FILE)
-    # for line in lf:
-    #     ner.add_label(line.rstrip())
-    # lf.close()
+    # add labels
+    with open(LABELS_FILE) as lf:
+        for line in lf:
+            ner.add_label(line.rstrip())
+        print("Added labels")
 
-    # # get names of other pipes to disable them during training
-    # pipe_exceptions = ["ner", "trf_wordpiecer", "trf_tok2vec"]
-    # other_pipes = [pipe for pipe in nlp.pipe_names if pipe not in pipe_exceptions]
-    # with nlp.disable_pipes(*other_pipes):  # only train NER
-    #     # reset and initialize the weights randomly – but only if we're
-    #     # training a new model
-    #     if model is None:
-    #         nlp.begin_training()
-    #     for itn in range(n_iter):
-    #         random.shuffle(train_data)
-    #         losses = {}
-    #         # batch up the examples using spaCy's minibatch
-    #         batches = minibatch(train_data, size=compounding(4.0, 32.0, 1.001))
-    #         for batch in batches:
-    #             texts, annotations = zip(*batch)
-    #             nlp.update(
-    #                 texts,  # batch of texts
-    #                 annotations,  # batch of annotations
-    #                 drop=0.5,  # dropout - make it harder to memorise data
-    #                 losses=losses,
-    #             )
-    #         print("Losses", losses)
+    # get names of other pipes to disable them during training
+    pipe_exceptions = ["ner", "trf_wordpiecer", "trf_tok2vec"]
+    other_pipes = [pipe for pipe in nlp.pipe_names if pipe not in pipe_exceptions]
+    with nlp.disable_pipes(*other_pipes):  # only train NER
+        # reset and initialize the weights randomly – but only if we're
+        # training a new model
+        if model is None:
+            nlp.begin_training()
+        for itn in range(n_iter):
+            random.shuffle(train_data)
+            losses = {}
+            # batch up the examples using spaCy's minibatch
+            batches = minibatch(train_data, size=compounding(4.0, 32.0, 1.001))
+            for batch in batches:
+                texts, annotations = zip(*batch)
+                nlp.update(
+                    texts,  # batch of texts
+                    annotations,  # batch of annotations
+                    drop=0.5,  # dropout - make it harder to memorise data
+                    losses=losses,
+                )
+            print("Losses", losses)
 
-    # # test the trained model
-    # for text, _ in train_data:
-    #     doc = nlp(text)
-    #     print("Entities", [(ent.text, ent.label_) for ent in doc.ents])
-    #     print("Tokens", [(t.text, t.ent_type_, t.ent_iob) for t in doc])
+    # test the trained model
+    for text, _ in train_data:
+        doc = nlp(text)
+        print("Entities", [(ent.text, ent.label_) for ent in doc.ents])
+        print("Tokens", [(t.text, t.ent_type_, t.ent_iob) for t in doc])
 
-    # # save model to output directory
-    # if output_dir is not None:
-    #     output_dir = Path(output_dir)
-    #     if not output_dir.exists():
-    #         output_dir.mkdir()
-    #     nlp.to_disk(output_dir)
-    #     print("Saved model to", output_dir)
+    # save model to output directory
+    if output_dir is not None:
+        output_dir = Path(output_dir)
+        if not output_dir.exists():
+            output_dir.mkdir()
+        nlp.to_disk(output_dir)
+        print("Saved model to", output_dir)
 
-    #     # test the saved model
-    #     print("Loading from", output_dir)
-    #     nlp2 = spacy.load(output_dir)
-    #     for text, _ in train_data:
-    #         doc = nlp2(text)
-    #         print("Entities", [(ent.text, ent.label_) for ent in doc.ents])
-    #         print("Tokens", [(t.text, t.ent_type_, t.ent_iob) for t in doc])
+        # # test the saved model
+        print("Loading from", output_dir)
+        nlp2 = spacy.load(output_dir)
+        for text, _ in train_data:
+            doc = nlp2(text)
+            print("Entities", [(ent.text, ent.label_) for ent in doc.ents])
+            print("Tokens", [(t.text, t.ent_type_, t.ent_iob) for t in doc])
 
 
 if __name__ == "__main__":
