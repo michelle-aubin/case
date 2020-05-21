d64783563964a0eb099fb22ea767db0af146e063
Michelle Aubin <phiangda@ualberta.ca>
2020-05-21 16:20:16 -0600

    changed patterns file

--- add_patterns_to_model.py
+++ add_patterns_to_model.py
@@ -6,8 +6,8 @@ from spacy.pipeline import EntityRuler
 # and from en_core_web_sm (ORG, DATE, GPE, etc)
 def main():
     nlp = spacy.load("en_core_sci_md", disable=["ner"])
+    ruler = EntityRuler(nlp).from_disk("patterns_wo_disease_or_syndrome.jsonl")
     web_nlp = spacy.load("en_core_web_sm", disable=["tagger","parser"])
-    ruler = EntityRuler(nlp).from_disk("patterns.jsonl")
     nlp.add_pipe(web_nlp.get_pipe("ner"), after="parser", name="web_ner")
     nlp.add_pipe(ruler, before="web_ner")
  
@@ -21,14 +21,14 @@ def main():
     for label in labels:
         nlp.vocab.strings.add(label)
 
-    text = """
-    During the study period, respiratory specimens (sputum, nasopharyngeal aspiration, endotracheal secretion, and bronchoalveolar lavage) for M. pneumoniae culture were obtained from patients with upper or lower respiratory tract infections seen as inpatients or in the outpatient or emergency departments. Respiratory specimens were aslo Gram-stained and cultured for bacteria and viruses. M. pneumoniae serological tests for IgG or IgM were not available at KAUH during the study period. All positive culture results were obtained from the Microbiology laboratory records. Charts of patients were reviewed with standardized data collection. Information collected included patients' demographics, comorbidities, clinical manifestations, complications, and outcome.
-       """
-    doc = nlp(text)
+    # text = """
+    # A person has Down syndrome. The body produces precipitation. During the study period, respiratory specimens (sputum, nasopharyngeal aspiration, endotracheal secretion, and bronchoalveolar lavage) for M. pneumoniae culture were obtained from patients with upper or lower respiratory tract infections seen as inpatients or in the outpatient or emergency departments. Respiratory specimens were aslo Gram-stained and cultured for bacteria and viruses. M. pneumoniae serological tests for IgG or IgM were not available at KAUH during the study period. All positive culture results were obtained from the Microbiology laboratory records. Charts of patients were reviewed with standardized data collection. Information collected included patients' demographics, comorbidities, clinical manifestations, complications, and outcome.
+    #    """
+    # doc = nlp(text)
 
-    for ent in doc.ents:
-        print(ent.text, ent.label_)
+    # for ent in doc.ents:
+    #     print(ent.text, ent.label_)
 
     # save model to disk
- #   nlp.to_disk("custom_model/")
+    nlp.to_disk("custom_model3/")
 main()
