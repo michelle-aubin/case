c8d0a45494d48149b2ddc49d0a9dd91893c221f0
Michelle Aubin <phiangda@ualberta.ca>
2020-05-25 16:23:08 -0600

    contains articles with text access and no duplicates

--- create_metadata.py
+++ create_metadata.py
@@ -3,11 +3,11 @@ import csv
 def main():
     with open("metadata.csv", "r", encoding="utf-8") as f_meta:
         with open("clean_metadata.csv", "w", encoding="utf-8") as f_out:
+            seen = set()
             f_out.write("cord_uid,json_file\n")
             articles = []
             metadata = csv.DictReader(f_meta)
             for row_id, row in enumerate(metadata):
-                print(row_id)
                 urls = []
                 pdf_url = row.get("pdf_json_files")
                 pmc_url = row.get("pmc_json_files")
@@ -16,7 +16,12 @@ def main():
                 elif pmc_url:
                     urls = pmc_url.split("; ")
                 if urls:
-                    out_str = row.get("cord_uid") + "," + urls[0] +"\n"
-                    f_out.write(out_str)
+                    cord_uid = row.get("cord_uid")
+                    if cord_uid not in seen:
+                        seen.add(cord_uid)
+                        out_str = cord_uid + "," + urls[0] +"\n"
+                        f_out.write(out_str)
+                    else:
+                        print("Duplicate %s" % cord_uid)
 
 main()
