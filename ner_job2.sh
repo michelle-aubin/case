44e01e7c6e0d20b76d34eef4e5b34368b8d29213
Michelle Aubin <phiangda@ualberta.ca>
2020-05-26 11:42:17 -0600

    creates venv

--- ner_job2.sh
+++ ner_job2.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
-#SBATCH --time=12:00:00
-python ner_tool.py 34211 58422 10 4
+#SBATCH --time=16:00:00
+module load python/3.7
+virtualenv --no-download $SLURM_TMPDIR/env
+source $SLURM_TMPDIR/env/bin/activate
+pip install --no-index --upgrade pip
+
+pip install --no-index -r requirements.txt
+python ner_tool.py 34211 58422 10 8
