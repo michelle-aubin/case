8178d1debff70d2e1af13c13179bc798d4b2cebe
Michelle Aubin <phiangda@cedar5.cedar.computecanada.ca>
2020-05-26 15:29:57 -0700

    good

--- ner_job1.sh
+++ ner_job1.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-#SBATCH --time=16:00:00
+#SBATCH --time=06:00:00
 #SBATCH --ntasks-per-node=1
-#SBATCH --cpus-per-task=48
-#SBATCH --mem-per-cpu=10G
+#SBATCH --cpus-per-task=32
+#SBATCH --mem=20G
 module load python/3.7
 virtualenv --no-download $SLURM_TMPDIR/env1
 source $SLURM_TMPDIR/env/bin/activate
 pip install --no-index --upgrade pip
 
 pip install --no-index -r requirements.txt
-python ner_tool.py 10710 11709 20 47
+python ner_tool.py 10710 11709 20 31
 
