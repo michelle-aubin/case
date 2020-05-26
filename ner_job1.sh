1b035d58e66046eedcae23a80c089213adb75f14
Michelle Aubin <phiangda@ualberta.ca>
2020-05-26 16:05:30 -0600

    using all cores

--- ner_job1.sh
+++ ner_job1.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 #SBATCH --time=16:00:00
+#SBATCH --ntasks-per-node=48
+#SBATCH --mem-per-cpu=10G
 module load python/3.7
-virtualenv --no-download $SLURM_TMPDIR/env
+virtualenv --no-download $SLURM_TMPDIR/env1
 source $SLURM_TMPDIR/env/bin/activate
 pip install --no-index --upgrade pip
 
 pip install --no-index -r requirements.txt
-python ner_tool.py 10000 34210 10 8
+python ner_tool.py 10710 11709 20 47
 
