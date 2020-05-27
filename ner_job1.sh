a3629eee7f8d9e6955f0651b17e336a8e686cf5d
Michelle Aubin <phiangda@ualberta.ca>
2020-05-26 18:16:42 -0600

    does remaining documents

--- ner_job1.sh
+++ ner_job1.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
-#SBATCH --time=06:00:00
-#SBATCH --ntasks-per-node=1
-#SBATCH --cpus-per-task=32
-#SBATCH --mem=20G
-module load python/3.7
-virtualenv --no-download $SLURM_TMPDIR/env1
+#SBATCH --time=12:00:00
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=48
+#SBATCH --mem=0
+module load python/3.6
+virtualenv --no-download $SLURM_TMPDIR/env
 source $SLURM_TMPDIR/env/bin/activate
 pip install --no-index --upgrade pip
 
 pip install --no-index -r requirements.txt
-python ner_tool.py 10710 11709 20 31
+python ner_tool.py 10710 34210 10 47
+
 
