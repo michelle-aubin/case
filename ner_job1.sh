5c7e645d8771eb5fe3067652009aad258dc285de
Michelle Aubin <phiangda@cedar5.cedar.computecanada.ca>
2020-05-26 17:22:39 -0700

    took out blank line

--- ner_job1.sh
+++ ner_job1.sh
@@ -7,6 +7,5 @@ module load python/3.6
 virtualenv --no-download $SLURM_TMPDIR/env
 source $SLURM_TMPDIR/env/bin/activate
 pip install --no-index --upgrade pip
-
 pip install --no-index -r requirements.txt
 python ner_tool.py 10710 34210 10 47
--- ner_job2.sh
+++ ner_job2.sh
@@ -7,6 +7,5 @@ module load python/3.6
 virtualenv --no-download $SLURM_TMPDIR/env
 source $SLURM_TMPDIR/env/bin/activate
 pip install --no-index --upgrade pip
-
 pip install --no-index -r requirements.txt
 python ner_tool.py 34211 58422 10 47
