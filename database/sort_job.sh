edf53359f0982fc6778dce9d5d4e118f86ca2248
Michelle Aubin <phiangda@ualberta.ca>
2020-06-01 15:09:28 -0600

    do job in virtualenv

--- database/sort_job.sh
+++ database/sort_job.sh
@@ -3,6 +3,10 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=8
 #SBATCH --mem=10G
-cd database/
+module load python/3.6
+virtualenv --no-download $SLURM_TMPDIR/env
+source $SLURM_TMPDIR/env/bin/activate
+pip install --no-index --upgrade pip
+pip install --no-index -r ../requirements.txt
 python get_non_entities.py
 sort -u --parallel=8 -o sorted.txt non_entities.txt 
