c06d10289d872bc07425955dc88c2a25e12bfa90
Michelle Aubin <phiangda@ualberta.ca>
2020-06-01 14:59:12 -0600

    use 10g of mem

--- database/sort_job.sh
+++ database/sort_job.sh
@@ -2,7 +2,7 @@
 #SBATCH --time=12:00:00
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=8
-#SBATCH --mem=0
+#SBATCH --mem=10G
 cd database/
 python get_non_entities.py
 sort -u --parallel=8 -o sorted.txt non_entities.txt 
