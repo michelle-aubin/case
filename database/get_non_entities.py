4fb11b24868a96de11bbb7c0334356519c71bfc0
Michelle Aubin <phiangda@ualberta.ca>
2020-06-01 14:45:57 -0600

    doesnt include sent id

--- database/get_non_entities.py
+++ database/get_non_entities.py
@@ -14,14 +14,14 @@ def main():
         c.execute("SELECT * FROM sentences;")
         for row in c:
             doc_id = row[0]
-            sent_id = row[1]
+           # sent_id = row[1]
             text = row[2]
             doc = nlp(text)
             for token in doc:
                 # only take non entities and non punctuation
                 # and take lowercase form
                 if token.ent_iob == 2 and not token.is_punct:
-                    output = token.lower_ + "|" + doc_id + "|" + str(sent_id) + "\n"
+                    output = token.lower_ + "|" + doc_id + "\n"
                     f_out.write(output)
     
 
