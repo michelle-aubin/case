cd80a81511e67848700cb0195fca8102a7b2c8ab
Michelle Aubin <phiangda@ualberta.ca>
2020-06-01 14:44:02 -0600

    write non_entities txt file and sort

--- database/sort_job.sh
+++ database/sort_job.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-#SBATCH --time=6:00:00
+#SBATCH --time=12:00:00
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=8
 #SBATCH --mem=0
+cd database/
+python get_non_entities.py
 sort -u --parallel=8 -o sorted.txt non_entities.txt 
