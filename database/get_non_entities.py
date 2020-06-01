4c03e317c087d670d35d68e0d9b148d368f2552c
Michelle Aubin <phiangda@ualberta.ca>
2020-06-01 13:06:09 -0600

    get lowercase of words that arent entities or punctuation

---
+++ database/get_non_entities.py
@@ -0,0 +1,29 @@
+import spacy
+import sqlite3
+
+def main():
+    conn = sqlite3.connect("cord19.db")
+    c = conn.cursor()
+    c.execute("PRAGMA foreign_keys = ON;")
+    conn.commit()
+
+    print("Loading model...")
+    nlp = spacy.load("../custom_model3")
+
+    with open("non_entities.txt", "w") as f_out:
+        c.execute("SELECT * FROM sentences;")
+        for row in c:
+            doc_id = row[0]
+            sent_id = row[1]
+            text = row[2]
+            print(text)
+            doc = nlp(text)
+            for token in doc:
+                # only take non entities and non punctuation
+                # and take lowercase form
+                if token.ent_iob == 2 and not token.is_punct:
+                    output = token.lower_ + "|" + doc_id + "|" + str(sent_id) + "\n"
+                    f_out.write(output)
+    
+
+main()
