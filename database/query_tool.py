d7788da8fd77ab48cf153c76e539e83e3291c5f5
Michelle Aubin <phiangda@ualberta.ca>
2020-06-01 23:05:20 -0600

    more testing

--- database/query_tool.py
+++ database/query_tool.py
@@ -8,23 +8,29 @@ def main():
     conn.commit()
 
     print("Loading model...")
-    nlp = spacy.load("../custom_model3")
+  #  nlp = spacy.load("../custom_model3")
     text = ""
-    while text != "quit":
-        text = input("Enter a question: ")
-        doc = nlp(text)
-        print("Entities found:")
-        for ent in doc.ents:
-            print("\t%s" % ent.text)
+
+    c.execute("select * from sentences;")
+    for row in c:
+        print(row)
+        break
+
+    # while text != "quit":
+    #     text = input("Enter a question: ")
+    #     doc = nlp(text)
+    #     print("Entities found:")
+    #     for ent in doc.ents:
+    #         print("\t%s" % ent.text)
         
-        for ent in doc.ents:
-            c.execute("""select DISTINCT s.doc_id, s.sentence 
-                        from sentences s, entities e 
-                        where s.doc_id = e.doc_id and s.sent_id = e.sent_id
-                        and e.entity = :ent;  """, {"ent":ent.text})
-            conn.commit()
-            sents = c.fetchall()
-            for sent in sents:
-                print(sent)
+    #     for ent in doc.ents:
+    #         c.execute("""select DISTINCT s.doc_id, s.sentence 
+    #                     from sentences s, entities e 
+    #                     where s.doc_id = e.doc_id and s.sent_id = e.sent_id
+    #                     and e.entity = :ent;  """, {"ent":ent.text})
+    #         conn.commit()
+    #         sents = c.fetchall()
+    #         for sent in sents:
+    #             print(sent)
 
 main()
