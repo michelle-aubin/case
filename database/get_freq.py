67c453a08218cdb3789cbe7d6433d348aa3f16ba
Michelle Aubin <phiangda@ualberta.ca>
2020-06-03 15:41:10 -0600

    freq in percent out of total num of docs

--- database/get_freq.py
+++ database/get_freq.py
@@ -13,13 +13,20 @@ def main():
             word_counts[word] +=1
             
     print("Building output...")
-    with open("frequencies.txt", "w", encoding="utf-8") as f_out:    
+    with open("frequencies3.txt", "w", encoding="utf-8") as f_out:    
         for word, count in sorted(word_counts.items(), key=lambda item: item[1], reverse=True):
-            output = "{:<8}{}\n".format(count, word)
+            output = "{:<8.2f}{}\n".format((count/58422)*100, word)
             f_out.write(output)
 
-        
 
+    # to count how many docs are in the input file
+    # doc_counts = defaultdict(lambda: 0)
+    # with open("tokens.txt", "r", encoding="utf-8") as f_in:
+    #     print("Reading file...")
+    #     for line in f_in:
+    #         doc = line.split("|")[1]
+    #         doc_counts[doc] = 0
+    # print("There are %d documents in tokens.txt" % len(doc_counts))
                 
 
 main()
