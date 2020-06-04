45441fe5f557970bf51c8ce74459d4af26757052
Michelle Aubin <phiangda@ualberta.ca>
2020-06-04 08:39:57 -0600

    split to smaller

--- database/sort_job5.sh
+++ database/sort_job5.sh
@@ -8,6 +8,7 @@ virtualenv --no-download $SLURM_TMPDIR/env
 source $SLURM_TMPDIR/env/bin/activate
 pip install --no-index --upgrade pip
 pip install --no-index -r ../requirements.txt
-python get_non_entities.py 40000 49999 10 47
-cat token-results/token40000-49999/*.txt > merged40000-49999.txt
-sort -u --parallel=8 -o merged40000-49999.txt merged40000-49999.txt
+python get_non_entities.py 50000 58422 10 47
+cat token-results/token50000-58422/*.txt > merged50000-58422.txt
+sort -u --parallel=8 -o merged50000-58422.txt merged50000-58422.txt
+
