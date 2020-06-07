ff72fc1705c92360af7ab1a6d4ebd36655a056d7
Michelle Aubin <phiangda@ualberta.ca>
2020-06-07 01:35:37 -0600

    try except and timed

--- database/insert_tokens.py
+++ database/insert_tokens.py
@@ -1,24 +1,11 @@
 import sqlite3
+import time
 
 def main():
+    start_time = time.time()
     conn = sqlite3.connect("cord19.db")
     c = conn.cursor()
     c.execute("PRAGMA foreign_keys = ON;")
-
-    c.executescript("""
-                    drop table if exists terms;
-                    """)
-
-    c.executescript("""
-                    create table terms (
-                        term      text,
-                        doc_id      char(8),
-                        sent_id     int,
-                        start       int,
-                        primary key (doc_id,sent_id,start),
-                        foreign key (doc_id,sent_id) references sentences
-                    );
-                    """)
     conn.commit()
 
     with open("stopWords.txt", "r", encoding="utf-8") as f_in:
@@ -29,8 +16,13 @@ def main():
             term, doc_id, sent_id, off_st, off_en = line.split("|!|")
             if term not in stop_words:
                 values = (term, doc_id, int(sent_id), int(off_st))
-                c.execute("insert into terms values (?, ?, ?, ?);", values)
+                try:
+                    c.execute("insert into terms values (?, ?, ?, ?);", values)
+                except Exception as e:
+                    print(e)
+                    print(values)
     conn.commit()
+    print("Took %s seconds" % (time.time() - start_time))
 
 
 main()
