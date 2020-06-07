7a781ea58080f133b55720870619fa835f55a676
Michelle Aubin <phiangda@ualberta.ca>
2020-06-07 01:34:18 -0600

    correct formula and skip stop words

--- database/get_freq.py
+++ database/get_freq.py
@@ -2,21 +2,26 @@ from collections import defaultdict
 import math
 
 def main():
+    with open("stopWords.txt", "r", encoding="utf-8") as f_in:
+        stop_words = {term.strip() for term in f_in}
+
     word_counts = defaultdict(lambda: 0)
     print("Reading file...")
-    with open("tokens.txt", "r", encoding="utf-8") as f_in:
+    with open("sorted_tokens.txt", "r", encoding="utf-8") as f_in:
         for line in f_in:
             word = line.split("|!|")[0]
             # word doesn't have any alpha characters - dont include
             if not word.islower():
                 continue
-            word_counts[word] +=1
+            if word not in stop_words:
+                word_counts[word] +=1
             
 
     print("Building output...")
     with open("idf.txt", "w", encoding="utf-8") as f_out:    
         for word, count in sorted(word_counts.items(), key=lambda item: item[1], reverse=True):
-            idf = math.log(count / 58419, 10)
+            # idf is log(total num of docs / num of docs that term appears in)
+            idf = math.log(58419 / count, 10)
             f_out.write(word + "|!|" + str(idf) + "\n")
 
 
