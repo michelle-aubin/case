ec71e015e91b1eedb15868c38ed94ed5036ffbea
Michelle Aubin <phiangda@ualberta.ca>
2020-06-07 00:08:08 -0600

    gets idf

--- database/get_freq.py
+++ database/get_freq.py
@@ -1,5 +1,5 @@
-import time
 from collections import defaultdict
+import math
 
 def main():
     word_counts = defaultdict(lambda: 0)
@@ -12,11 +12,12 @@ def main():
                 continue
             word_counts[word] +=1
             
+
     print("Building output...")
-    with open("frequencies3.txt", "w", encoding="utf-8") as f_out:    
+    with open("idf.txt", "w", encoding="utf-8") as f_out:    
         for word, count in sorted(word_counts.items(), key=lambda item: item[1], reverse=True):
-            output = "{:<8.2f}{}\n".format((count/58422)*100, word)
-            f_out.write(output)
+            idf = math.log(count / 58419, 10)
+            f_out.write(word + "|!|" + str(idf) + "\n")
 
 
     # to count how many docs are in the input file
