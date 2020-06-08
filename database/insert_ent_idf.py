8eab93ce3b41f97116b0d39953d1e04fb0c48580
Michelle Aubin <phiangda@ualberta.ca>
2020-06-08 11:02:59 -0600

    creates and populates idf table for entities

---
+++ database/insert_ent_idf.py
@@ -0,0 +1,31 @@
+import sqlite3
+
+# ent_idf(entity, idf)
+def main():
+    conn = sqlite3.connect("cord19.db")
+    c = conn.cursor()
+    c2 = conn.cursor()
+    c.execute("PRAGMA foreign_keys = ON;")
+    conn.commit()
+
+    c.executescript("""
+                    drop table if exists ent_idf;
+                    create table ent_idf (
+                        entity      text,
+                        idf       double,
+                        primary key (entity)
+                    );
+                    """)
+    conn.commit()
+
+    c.execute(""" select entity, count(distinct doc_id)
+                from entities
+                group by entity
+        """)
+
+    for row in c:
+        values = (row[0], row[1])
+        c2.execute("insert into ent_idf values (?, ?);", values)
+    conn.commit()
+main()
+
