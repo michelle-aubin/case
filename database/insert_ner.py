4fcf4788a6e6419082022d69585815adf6c2674c
Michelle Aubin <phiangda@ualberta.ca>
2020-06-09 13:09:07 -0600

    creates table too

--- database/insert_ner.py
+++ database/insert_ner.py
@@ -7,8 +7,24 @@ def main():
     c = conn.cursor()
     c.execute("PRAGMA foreign_keys = ON;")
     conn.commit()
+
+    drop_start = time.time()
+    c.executescript("""
+                drop table if exists entities;
+                create table entities (
+                                    entity      text,
+                                    type        char(35),
+                                    doc_id      char(8),
+                                    sent_id     int,
+                                    start       int,
+                                    primary key (doc_id,sent_id,start),
+                                    foreign key (doc_id,sent_id) references sentences
+                                );
+                """)
+    print("Dropping old table took %s seconds" % (time.time() - drop_start))
     # iterate through files in sentences directory
   #  os.chdir('../sentences')
+    insert_time = time.time()
     for root, dirs, files in os.walk("../ner-results"):
         for name in files:
             data = []
@@ -26,6 +42,7 @@ def main():
                         print(e)
                         print("Bad line: ", entry)
         conn.commit()
+    print("Populating table took %s seconds" % (time.time() - insert_time))
 
 
 main()
