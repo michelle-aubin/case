c37ad26ef8e82c9f3e717178232b8003ab7f22e7
Michelle Aubin <phiangda@ualberta.ca>
2020-06-10 14:17:19 -0600

    outputs top 10 docs, their score, and link to full article

--- database/query_tool.py
+++ database/query_tool.py
@@ -1,6 +1,8 @@
 import sqlite3
 import spacy
 from bm25 import get_score
+import time
+from constants import URL
 import csv
 
 def main():
@@ -10,48 +12,59 @@ def main():
     conn.commit()
 
     stop_words = set()
-    # with open("stopWords.txt", "r") as f_stop:
-    #     for line in f_stop:
-    #         stop_words.add(line.strip())
     c.execute("select term from stop_words;")
     for row in c:
         stop_words.add(row[0])
+
+    # get links to json files of the docs
+    urls = {}
+    with open("../clean_metadata.csv", "r", encoding="utf-8") as f_meta:
+        metadata = csv.DictReader(f_meta)
+        for row in metadata:
+            urls[row.get("cord_uid")] = row.get("json_file")
 
     print("Loading model...")
     nlp = spacy.load("../custom_model3")
     text = ""
 
+    text = input("Enter a question: ")
     while text != "quit":
         terms = []
         entities = []
-        text = input("Enter a question: ")
         doc = nlp(text)
         print("Entities found:")
         for ent in doc.ents:
             print("\t%s" % ent.text)
-            entities.append(ent.text)
+            entities.append(ent.text.lower())
 
         print("Tokens found:")
         for token in doc:
           # if token is a non entity and not a punct and not a stop word
-          if token.ent_iob == 2 and not token.is_punct and token.text not in stop_words:
+          if token.ent_iob == 2 and not token.is_punct and token.text.lower() not in stop_words:
             print("\t%s" % token.text)
-            terms.append(token.text)
-
+            terms.append(token.text.lower())
         # terms = ["antiviral", "treatment"]
 
+        print("Getting scores...")
+        start = time.time()
         # get docs that have all of the entities and tokens? or that have at least one? or just do all docs?
         doc_scores = {}
         with open("doc_ids.txt", "r", encoding="utf-8") as f_docs:
             for row in f_docs:
                 doc_id = row.strip()
                 doc_scores[doc_id] = get_score(doc_id, terms, entities)
+        print("Took %.2f seconds to get scores" % (time.time() - start))
         i = 0
         for doc, score in sorted(doc_scores.items(), key=lambda item: item[1], reverse=True):
-            if i > 10:
+            if i > 5:
                 break
             i += 1
-            print(doc, score)
+            print("Doc ID: %s       Score: %.4f     Link: %s" % (doc, score, URL + urls[doc]))
+        # print("Scores of top docs from kaggle engine:")
+        # print("sasijnks", doc_scores["sasijnks"])
+        # print("sasijnks", doc_scores["itz0bdrc"])
+        text = input("Enter a question: ")
+
 
 
 
