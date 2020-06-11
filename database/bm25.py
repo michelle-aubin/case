ec477fbc3c509a47d5604991e23f38bfc3832497
Michelle Aubin <phiangda@ualberta.ca>
2020-06-11 10:58:59 -0600

    changed to bm25+

--- database/bm25.py
+++ database/bm25.py
@@ -1,6 +1,6 @@
 import sqlite3
 import math
-from constants import AVG_DOC_LENGTH, BM25_B, BM25_K1, TOTAL_DOCS
+from constants import AVG_DOC_LENGTH, BM25_B, BM25_K1, TOTAL_DOCS, BM25_delta
 
 
 # Returns BM25 score of a document
@@ -15,28 +15,38 @@ def get_score(doc_id, terms, entities):
 
     c.execute("select length from doc_lengths where doc_id = :doc_id;", {"doc_id":doc_id})
     doc_length = c.fetchone()[0]
+    print("For Doc %s:" % doc_id)
+    print("\tLength: %d" % doc_length)
 
     score = 0
+    print("\tScore: %f" % score)
     for term in terms:
+        print("\tFor term \"%s\":" % term)
         # get idf of the term
         c.execute("select idf from idf where term = :term;", {"term": term})
         result = c.fetchone()
         idf = result[0] if result else get_idf(0)
+        print("\t\tIDF: %f" % idf)
         # get tf of the term in the doc
         c.execute("select frequency from tf where term = :term and doc_id = :doc_id;", {"term": term, "doc_id":doc_id})
         result = c.fetchone()
         tf = result[0] if result else 0
+        print("\t\tTF: %f" % tf)
         # calculate score
         score += calc_summand(tf, idf, doc_length)
+        print("\tScore: %f" % score)
     for ent in entities:
+        print("\tFor entity \"%s\":" % ent)
         # get idf of the entity
         c.execute("select idf from ent_idf where entity = :entity;", {"entity": ent})
         result = c.fetchone()
         idf = result[0] if result else get_idf(0)
+        print("\t\tIDF: %f" % idf)
         # get tf of the entity in the doc
         c.execute("select frequency from ent_tf where entity = :entity and doc_id = :doc_id;", {"entity": ent, "doc_id":doc_id})
         result = c.fetchone()
         tf = result[0] if result else 0
+        print("\t\tTF: %f" % tf)
         # calculate score
         score += calc_summand(tf, idf, doc_length)
     
@@ -48,8 +58,10 @@ def get_score(doc_id, terms, entities):
 # doc_length: the length of the doc in words
 def calc_summand(tf, idf, doc_length):
     numerator = tf * (BM25_K1 + 1)
+    print("\t\tNumerator: %f" % numerator)
     denominator = tf + BM25_K1 * (1 - BM25_B + BM25_B * (doc_length / AVG_DOC_LENGTH))
-    return idf * (numerator / denominator)
+    print("\t\tDenominator: %f" % denominator)
+    return idf * ((numerator / denominator) + BM25_delta)
 
 
 # Calculates and returns idf given the number of docs containing a term
