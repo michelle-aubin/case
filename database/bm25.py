26699fa92c050bb8a7c1e30dcae009b62d83534e
Michelle Aubin <phiangda@ualberta.ca>
2020-06-12 13:37:05 -0600

    added prints for debugging

--- database/bm25.py
+++ database/bm25.py
@@ -15,6 +15,79 @@ def get_score(doc_id, terms, entities):
 
     c.execute("select length from doc_lengths where doc_id = :doc_id;", {"doc_id":doc_id})
     doc_length = c.fetchone()[0]
+#    print("For Doc %s:" % doc_id)
+#    print("\tLength: %d" % doc_length)
+
+    score = 0
+#    print("\tScore: %f" % score)
+    for term in terms:
+#        print("\tFor term \"%s\":" % term)
+        # get idf of the term
+        c.execute("select idf from idf where term = :term;", {"term": term})
+        result = c.fetchone()
+        idf = result[0] if result else get_idf(0)
+#        print("\t\tIDF: %f" % idf)
+        # get tf of the term in the doc
+        c.execute("select frequency from tf where term = :term and doc_id = :doc_id;", {"term": term, "doc_id":doc_id})
+        result = c.fetchone()
+        tf = result[0] if result else 0
+#        print("\t\tTF: %f" % tf)
+        # calculate score
+        score += calc_summand(tf, idf, doc_length)
+#        print("\tScore: %f" % score)
+    for ent in entities:
+#        print("\tFor entity \"%s\":" % ent)
+        # get idf of the entity
+        c.execute("select idf from ent_idf where entity = :entity;", {"entity": ent})
+        result = c.fetchone()
+        idf = result[0] if result else get_idf(0)
+#        print("\t\tIDF: %f" % idf)
+        # get tf of the entity in the doc
+        c.execute("select frequency from ent_tf where entity = :entity and doc_id = :doc_id;", {"entity": ent, "doc_id":doc_id})
+        result = c.fetchone()
+        tf = result[0] if result else 0
+#        print("\t\tTF: %f" % tf)
+        # calculate score
+        score += calc_summand(tf, idf, doc_length)
+#        print("\tScore: %f" % score)
+    
+    return score
+
+# Calculates and returns the summand for one query term of bm25 formula
+# tf: the term frequency of the term in the doc
+# idf: the inverse document frequency of the term
+# doc_length: the length of the doc in words
+def calc_summand(tf, idf, doc_length):
+    numerator = tf * (BM25_K1 + 1)
+#    print("\t\tNumerator: %f" % numerator)
+    denominator = tf + BM25_K1 * (1 - BM25_B + BM25_B * (doc_length / AVG_DOC_LENGTH))
+#    print("\t\tDenominator: %f" % denominator)
+    return idf * ((numerator / denominator) + BM25_delta)
+
+
+# Calculates and returns idf given the number of docs containing a term
+# count: num of docs containing the term
+def get_idf(count):
+    # idf is log(           total num of docs + 1                    )
+    #           (----------------------------------------------------)
+    #           (        num of docs containing the term             )
+    numerator = TOTAL_DOCS + 1
+    denominator = count
+    idf = math.log(numerator/denominator)
+    return idf
+
+
+
+
+
+def print_get_score(doc_id, terms, entities):
+    conn = sqlite3.connect("cord19.db")
+    c = conn.cursor()
+    c.execute("PRAGMA foreign_keys = ON;")
+    conn.commit()
+
+    c.execute("select length from doc_lengths where doc_id = :doc_id;", {"doc_id":doc_id})
+    doc_length = c.fetchone()[0]
     print("For Doc %s:" % doc_id)
     print("\tLength: %d" % doc_length)
 
@@ -33,7 +106,7 @@ def get_score(doc_id, terms, entities):
         tf = result[0] if result else 0
         print("\t\tTF: %f" % tf)
         # calculate score
-        score += calc_summand(tf, idf, doc_length)
+        score += print_calc_summand(tf, idf, doc_length)
         print("\tScore: %f" % score)
     for ent in entities:
         print("\tFor entity \"%s\":" % ent)
@@ -48,29 +121,14 @@ def get_score(doc_id, terms, entities):
         tf = result[0] if result else 0
         print("\t\tTF: %f" % tf)
         # calculate score
-        score += calc_summand(tf, idf, doc_length)
+        score += print_calc_summand(tf, idf, doc_length)
+        print("\tScore: %f" % score)
     
     return score
 
-# Calculates and returns the summand for one query term of bm25 formula
-# tf: the term frequency of the term in the doc
-# idf: the inverse document frequency of the term
-# doc_length: the length of the doc in words
-def calc_summand(tf, idf, doc_length):
+def print_calc_summand(tf, idf, doc_length):
     numerator = tf * (BM25_K1 + 1)
     print("\t\tNumerator: %f" % numerator)
     denominator = tf + BM25_K1 * (1 - BM25_B + BM25_B * (doc_length / AVG_DOC_LENGTH))
     print("\t\tDenominator: %f" % denominator)
     return idf * ((numerator / denominator) + BM25_delta)
-
-
-# Calculates and returns idf given the number of docs containing a term
-# count: num of docs containing the term
-def get_idf(count):
-    # idf is log(           total num of docs + 1                    )
-    #           (----------------------------------------------------)
-    #           (        num of docs containing the term             )
-    top = TOTAL_DOCS + 1
-    bottom = count
-    idf = math.log(top/bottom)
-    return idf
