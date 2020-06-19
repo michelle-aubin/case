308da7345a49207031a4044e80934759e9b794b9
Michelle Aubin <phiangda@ualberta.ca>
2020-06-18 22:57:48 -0600

    works well

--- database/task-results/kendall_tau_distance.py
+++ database/task-results/kendall_tau_distance.py
@@ -1,21 +1,32 @@
 import itertools
 
-def main():
-    rank1 = ["urs5z4l1", "mcyeyl4s", "trcisop6", "js2ppypr", "6g1y3eyy"]
-    rank2 = ["urs5z4l1","mcyeyl4s","js2ppypr","xiv9vxdp","mgz7w9y4"]
-    all_docs = # intersection with no repetition
-    # get all pairs from all_docs (can use itertools.combinations)
-    # for each pair, get indices of the two items in the pair from both
-    # ranked lists
-    # if item1 index > item2 index in both lists, good
-    # else add one to distance
-    # if an item is not in a list, give it index 6
-    kendall_tau_distance(rank1,rank2)
-
+# Returns kendall tau distance given two ranked lists
 def kendall_tau_distance(rank1, rank2):
-    pairs = itertools.combinations(range(1, len(rank1)+1), 2)
+    # get all pairs
+    all_docs = list(set(rank1) | set(rank2))
+    pairs = itertools.combinations(all_docs, 2)
     distance = 0
-    for i, j in pairs:
-        a = 
+    for doc1, doc2 in pairs:
+        # print("For pair %s and %s:" % (doc1, doc2))
+        # print("\tList1: %s rank %d \t %s rank %d" % (doc1, get_rank(doc1, rank1), doc2, get_rank(doc2, rank1)))
+        # print("\tList2: %s rank %d \t %s rank %d" % (doc1, get_rank(doc1, rank2), doc2, get_rank(doc2, rank2)))
+        a = get_rank(doc1, rank1) - get_rank(doc2, rank1)
+        b = get_rank(doc1, rank2) - get_rank(doc2, rank2)
+        # print("\ta = %d \t b = %d" % (a,b))
+        # if a and b are diff signs then pair is discordant
+        if (a * b) < 0:
+            # print("\tDiscordant")
+            distance += 1
+    # print("Distance: %d" % distance)
+    return distance
+        
 
-main()
+# gets the rank of doc in the rank list
+# if doc is not in the list, returns a rank of 5 (outside of list)
+def get_rank(doc, rank_list):
+    try:
+        rank = rank_list.index(doc)
+    except ValueError:
+        rank = 5
+    finally:
+        return rank
