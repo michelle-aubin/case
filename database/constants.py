3ca5f31d0c0783f093a93c5775b235fba743c3d7
Michelle Aubin <phiangda@ualberta.ca>
2020-06-26 17:23:55 -0600

    updated for 2020-06-19 version of corpus

--- database/constants.py
+++ database/constants.py
@@ -1,16 +1,18 @@
+# name of sqlite database
+DB_NAME = "cord19.db"
 # total num of docs in the database
-TOTAL_DOCS = 58416
-
-# total num of rows in clean_metadata.csv
-METADATA_ROWS = 58422
+TOTAL_DOCS = 73861
+# average document length in words
+AVG_DOC_LENGTH = 4434
 
 # parameters for BM25 ranking function
 BM25_B = 0.3
 BM25_K1 = 1.2
 BM25_delta = 1
 
-# average document length in words
-AVG_DOC_LENGTH = 4904
 
 # base url for json files of the documents in CORD-19 dataset
 URL = "https://ai2-semanticscholar-cord-19.s3-us-west-2.amazonaws.com/latest/"
+
+# separator used in .txt files to populate db
+SEP = "|!|"
