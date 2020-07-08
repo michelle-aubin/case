888aed74809c2781afccad69708a8ab93265c8a8
Michelle Aubin <phiangda@ualberta.ca>
2020-07-08 14:25:47 -0600

    added params for proximity score

--- database/constants.py
+++ database/constants.py
@@ -1,15 +1,11 @@
-# name of sqlite database
-DB_NAME = "cord19.db"
-# total num of docs in the database
-TOTAL_DOCS = 73861
-# average document length in words
-AVG_DOC_LENGTH = 4434
-
 # parameters for BM25 ranking function
 BM25_B = 0.3
 BM25_K1 = 1.2
 BM25_delta = 1
 
+# parameters for proximity score function
+PROX_K = 20
+PROX_R = 0.75
 
 # base url for json files of the documents in CORD-19 dataset
 URL = "https://ai2-semanticscholar-cord-19.s3-us-west-2.amazonaws.com/latest/"
