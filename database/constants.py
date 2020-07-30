077b7e65bf7ebf0c2e21c0f576e98c2b8df1df2e
Michelle Aubin <phiangda@ualberta.ca>
2020-07-30 14:06:42 -0600

    docs k is 25

--- database/constants.py
+++ database/constants.py
@@ -8,7 +8,7 @@ PROX_K = 20
 PROX_R = 0.75
 
 # the num of docs from the posting lists to rank 
-DOCS_K = 1000
+DOCS_K = 25
 
 # base url for json files of the documents in CORD-19 dataset
 URL = "https://ai2-semanticscholar-cord-19.s3-us-west-2.amazonaws.com/latest/"
