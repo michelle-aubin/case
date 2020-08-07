3ceea6b5b3c90e53d1b76be38e2254fc0cc911ab
Michelle Aubin <phiangda@ualberta.ca>
2020-08-07 10:06:00 -0600

    doesnt need to query db to get total num of docs

--- database/query_tool.py
+++ database/query_tool.py
@@ -65,9 +65,6 @@ def main(input_file, output_file, run_tag, db_name):
 
     # get queries from input file
     queries = get_queries(input_file)
-    # get total num of docs
-    c.execute("select count(doc_id) from doc_lengths;")
-    total_docs = c.fetchone()[0]
     # get average doc length
     c.execute("select avg(length) from doc_lengths;")
     avg_length = c.fetchone()[0]
@@ -78,6 +75,8 @@ def main(input_file, output_file, run_tag, db_name):
     c.execute("select doc_id, length from doc_lengths;")
     for row in c:
         doc_lengths[row[0]] = row[1]
+    # get total num of docs
+    total_docs = len(doc_lengths)
 
     print("Loading model...")
     nlp = spacy.load("../custom_model3")# , disable=['bc5cdr_ner', 'bionlp13cg_ner', 'entity_ruler', 'web_ner'])
